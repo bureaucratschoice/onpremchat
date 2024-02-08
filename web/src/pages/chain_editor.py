76a179d507c4c9f0198be18d7ead4d568f84fa50
bureaucratschoice <152599487+bureaucratschoice@users.noreply.github.com>
2024-02-08 13:40:53 +0000

    new way of storing chain

--- web/src/pages/chain_editor.py
+++ web/src/pages/chain_editor.py
@@ -9,17 +9,19 @@ def chain_editor(cfg,app):
         @ui.refreshable
         def chain() -> None:
             assign_uuid_if_missing(app)
-            i = 0
-            print(app.storage.user)            
-            while 'chain'+str(i) in app.storage.user:
-                print(app.storage.user['chain'+str(i)])
-                ui.chat_message(text=app.storage.user['chain'+str(i)], name="Sie")
-                    
-                i += 1
+            if 'chain' in app.storage.user:
           
-            
+            for chain_elem in app.storage.user['chain']:
+                print(chain_elem)
+                ui.chat_message(text=chain_elem, name="Sie",sent = True)
+                        
         def append() -> None:
             assign_uuid_if_missing(app)
+            chain_elem = app.storage.user['chain_elem']
+            if 'chain' in app.storage.user:
+                app.storage.user['chain'].append(chain_elem)
+            else:
+                app.storage.user['chain'] = [chain_elem]
             chain.refresh()
 
         anchor_style = r'a:link, a:visited {color: inherit !important; text-decoration: none; font-weight: 500}'
@@ -35,7 +37,7 @@ def chain_editor(cfg,app):
             with ui.column().classes('w-full max-w-3xl mx-auto my-6'):
                 with ui.row().classes('w-full no-wrap items-center'):
                     text = ui.textarea(placeholder=placeholder).props('rounded outlined input-class=mx-3').props('clearable') \
-                    .classes('w-full self-center').bind_value(app.storage.user, 'chain'+str(0)).on('keydown.enter', append)
+                    .classes('w-full self-center').bind_value(app.storage.user, 'chain_elem').on('keydown.enter', append)
                 send_btn = ui.button(icon="send", on_click=lambda: append())
 
            
