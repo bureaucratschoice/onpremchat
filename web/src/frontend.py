77ef63165b313817daf5a391ce338ab985fc75af
Philip Kossack <p.kossack@posteo.de>
2025-03-12 12:52:13 +0100

    fix login

--- web/src/frontend.py
+++ web/src/frontend.py
@@ -322,7 +322,22 @@ def init(fastapi_app: FastAPI, jobStat, taskQueue, cfg, statistic) -> None:
                 ui.table(columns=columns, rows=rows, row_key='name')
     
 
-        
+    @ui.page('/login')
+    def login() -> Optional[RedirectResponse]:
+        def try_login() -> None:  # local function to avoid passing username and password as arguments
+            if passwords.get(username.value) == password.value:
+                app.storage.user.update({'username': username.value, 'authenticated': True})
+                ui.navigate.to(app.storage.user.get('referrer_path', '/'))  # go back to where the user wanted to go
+            else:
+                ui.notify('Wrong username or password', color='negative')
+
+        if app.storage.user.get('authenticated', False):
+            return RedirectResponse('/')
+        with ui.card().classes('absolute-center'):
+            username = ui.input('Username').on('keydown.enter', try_login)
+            password = ui.input('Password', password=True, password_toggle_button=True).on('keydown.enter', try_login)
+            ui.button('Log in', on_click=try_login)
+        return None
 
     @ui.page('/pdf')
     def pdfpage():
